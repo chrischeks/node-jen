f91da34c444b75aea1993144a2fa6e1abf502c2e
Chekwube <chekwube@rubikpay.net>
2021-09-05 17:35:04 +0100

    Changed code in index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-  res.send("hello world");
+  res.send("hey world");
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
 
 module.exports = app;
